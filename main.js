eee7d1484129537ba6a444b642760f71ee6bc84a
DionisisLiontos <DionisisLiontos@outlook.com>
2023-05-16 03:37:53 +0300

    updated html and js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-document.addEventListener("DOMContentLoaded" , function(){
-    let screen = document.querySelector(".screen");
-    let clear = document.querySelector(".clear");
-    let equal = document.querySelector(".equals");
-    let float = document.querySelector(".float");
-    let numbers = document.querySelectorAll(".number");
-    let operators = document.querySelectorAll(".operator");
-})
+let screen = document.querySelector(".screen");
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equals");
+let float = document.querySelector(".float");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let firstNumber= "";
+let secondNumber = "";
+let operator = "+";
+
+
 function add(a,b){
     return a+b;
 }
@@ -23,9 +26,20 @@ function divide(a,b){
     return a/b;
 }
 
-let a;
-let b;
-let operator = "";
-let screen = document.querySelector(".screen");
-let clear = document.querySelector(".clear");
+
+clear.addEventListener("click", function(){
+    screen.textContent = "";
+})
+
+function operate(operator , firstNumber , secondNumber){
+    if(operator == "+"){
+        add(firstNumber,secondNumber)
+    }else if(operator == "-"){
+        subtract(firstNumber , secondNumber)
+    }else if (operator == "x"){
+        multiply(firstNumber , secondNumber)
+    }else if (operator == "/"){
+        divide(firstNumber,secondNumber)
+    }
+}
 
